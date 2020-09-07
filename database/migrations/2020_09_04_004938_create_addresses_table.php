f1d289cc53f723157c573eb222184f4777e0ba9f
muizzyusuf <43257447+muizzyusuf@users.noreply.github.com>
2020-09-06 21:49:50 -0700

    Add files via upload

---
+++ database/migrations/2020_09_04_004938_create_addresses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('addressId');
+            $table->string('address');
+            $table->string('city');
+            $table->char('zipcode',10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
